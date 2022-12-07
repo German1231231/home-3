c4c7f06f21438e5a8bfff33b7c68d1f446d8d7b7
German <german.glumov@gmail.com>
2022-12-07 23:34:37 +0300

    три

---
+++ 03/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите число от 1 до 10: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+int a = 0;
+
+if(n >= 1 && n < 10)
+{
+    for(int i = 1; i <= n; i++)
+    {
+        array[a] = i * i * i;
+        a++;
+    }
+}
+
+else
+{
+    Console.WriteLine("Введено неверное число");
+}
+
+Console.WriteLine("[{0}]", string.Join(", ", array));
