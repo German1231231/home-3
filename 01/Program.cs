dc6c01ca1a39d5e73a9516bdfd274346b95d9570
German <german.glumov@gmail.com>
2022-12-07 23:34:15 +0300

    one

---
+++ 01/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n > 9999 && n < 100000)
+{
+    if(n / 10000 == n % 10 && ((n / 1000) % 10) == ((n % 100) / 10))
+    {
+        Console.WriteLine("Является палиндромом");
+    }
+
+    else 
+    {
+        Console.WriteLine("Не является палиндромом");
+    }
+}
+
+else 
+{
+    Console.Write("Введенно не пятизначное число");
+}    
